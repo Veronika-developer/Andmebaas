149076badf4049468e599ac6885389d88997ebcf
Veronika <veronika.jefimova03@gmai.com>
2020-12-05 13:04:20 +0200

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,42 +35,41 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_insert = new System.Windows.Forms.Button();
             this.btn_Update = new System.Windows.Forms.Button();
             this.btn_Delete = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.toodenimetusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.kogusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.hindDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tootetableBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.toodedDataSet = new andmebaasid.ToodedDataSet();
             this.tootetableTableAdapter = new andmebaasid.ToodedDataSetTableAdapters.TootetableTableAdapter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Lisapilt = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tootetableBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.toodedDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Toodetxt
             // 
-            this.Toodetxt.Location = new System.Drawing.Point(98, 37);
-            this.Toodetxt.Margin = new System.Windows.Forms.Padding(4);
+            this.Toodetxt.Location = new System.Drawing.Point(108, 50);
+            this.Toodetxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Toodetxt.Name = "Toodetxt";
-            this.Toodetxt.Size = new System.Drawing.Size(132, 22);
+            this.Toodetxt.Size = new System.Drawing.Size(135, 22);
             this.Toodetxt.TabIndex = 0;
             // 
             // Kogustxt
             // 
-            this.Kogustxt.Location = new System.Drawing.Point(98, 75);
-            this.Kogustxt.Margin = new System.Windows.Forms.Padding(4);
+            this.Kogustxt.Location = new System.Drawing.Point(108, 90);
+            this.Kogustxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Kogustxt.Name = "Kogustxt";
             this.Kogustxt.Size = new System.Drawing.Size(132, 22);
             this.Kogustxt.TabIndex = 1;
             // 
             // Hindtxt
             // 
-            this.Hindtxt.Location = new System.Drawing.Point(98, 118);
-            this.Hindtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.Hindtxt.Location = new System.Drawing.Point(108, 130);
+            this.Hindtxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Hindtxt.Name = "Hindtxt";
             this.Hindtxt.Size = new System.Drawing.Size(132, 22);
             this.Hindtxt.TabIndex = 2;
@@ -79,7 +78,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 40);
+            this.label1.Location = new System.Drawing.Point(51, 55);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 17);
@@ -89,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 80);
+            this.label2.Location = new System.Drawing.Point(50, 95);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 17);
@@ -99,30 +98,30 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 123);
+            this.label3.Location = new System.Drawing.Point(52, 133);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Hind";
             // 
-            // btn_Insert
+            // btn_insert
             // 
-            this.btn_Insert.Location = new System.Drawing.Point(68, 186);
-            this.btn_Insert.Margin = new System.Windows.Forms.Padding(4);
-            this.btn_Insert.Name = "btn_Insert";
-            this.btn_Insert.Size = new System.Drawing.Size(87, 39);
-            this.btn_Insert.TabIndex = 6;
-            this.btn_Insert.Text = "Lisa uus";
-            this.btn_Insert.UseVisualStyleBackColor = true;
-            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            this.btn_insert.Location = new System.Drawing.Point(53, 201);
+            this.btn_insert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(105, 39);
+            this.btn_insert.TabIndex = 6;
+            this.btn_insert.Text = "Lisa uus";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
             // 
             // btn_Update
             // 
-            this.btn_Update.Location = new System.Drawing.Point(68, 248);
-            this.btn_Update.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Update.Location = new System.Drawing.Point(166, 201);
+            this.btn_Update.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Update.Name = "btn_Update";
-            this.btn_Update.Size = new System.Drawing.Size(87, 40);
+            this.btn_Update.Size = new System.Drawing.Size(103, 39);
             this.btn_Update.TabIndex = 7;
             this.btn_Update.Text = "Uuenda";
             this.btn_Update.UseVisualStyleBackColor = true;
@@ -130,64 +129,25 @@
             // 
             // btn_Delete
             // 
-            this.btn_Delete.Location = new System.Drawing.Point(68, 314);
-            this.btn_Delete.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Delete.Location = new System.Drawing.Point(54, 266);
+            this.btn_Delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Delete.Name = "btn_Delete";
-            this.btn_Delete.Size = new System.Drawing.Size(87, 38);
+            this.btn_Delete.Size = new System.Drawing.Size(103, 36);
             this.btn_Delete.TabIndex = 8;
             this.btn_Delete.Text = "Kustuta";
             this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.idDataGridViewTextBoxColumn,
-            this.toodenimetusDataGridViewTextBoxColumn,
-            this.kogusDataGridViewTextBoxColumn,
-            this.hindDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.tootetableBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(261, 13);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Location = new System.Drawing.Point(318, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(758, 385);
+            this.dataGridView1.Size = new System.Drawing.Size(751, 562);
             this.dataGridView1.TabIndex = 9;
             this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
-            // 
-            // idDataGridViewTextBoxColumn
-            // 
-            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
-            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
-            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
-            this.idDataGridViewTextBoxColumn.ReadOnly = true;
-            this.idDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // toodenimetusDataGridViewTextBoxColumn
-            // 
-            this.toodenimetusDataGridViewTextBoxColumn.DataPropertyName = "Toodenimetus";
-            this.toodenimetusDataGridViewTextBoxColumn.HeaderText = "Toodenimetus";
-            this.toodenimetusDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.toodenimetusDataGridViewTextBoxColumn.Name = "toodenimetusDataGridViewTextBoxColumn";
-            this.toodenimetusDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // kogusDataGridViewTextBoxColumn
-            // 
-            this.kogusDataGridViewTextBoxColumn.DataPropertyName = "Kogus";
-            this.kogusDataGridViewTextBoxColumn.HeaderText = "Kogus";
-            this.kogusDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.kogusDataGridViewTextBoxColumn.Name = "kogusDataGridViewTextBoxColumn";
-            this.kogusDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // hindDataGridViewTextBoxColumn
-            // 
-            this.hindDataGridViewTextBoxColumn.DataPropertyName = "Hind";
-            this.hindDataGridViewTextBoxColumn.HeaderText = "Hind";
-            this.hindDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.hindDataGridViewTextBoxColumn.Name = "hindDataGridViewTextBoxColumn";
-            this.hindDataGridViewTextBoxColumn.Width = 125;
             // 
             // tootetableBindingSource
             // 
@@ -203,27 +163,50 @@
             // 
             this.tootetableTableAdapter.ClearBeforeFill = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 310);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 263);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Lisapilt
+            // 
+            this.btn_Lisapilt.Location = new System.Drawing.Point(169, 266);
+            this.btn_Lisapilt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Lisapilt.Name = "btn_Lisapilt";
+            this.btn_Lisapilt.Size = new System.Drawing.Size(100, 36);
+            this.btn_Lisapilt.TabIndex = 11;
+            this.btn_Lisapilt.Text = "Lisa pilt";
+            this.btn_Lisapilt.UseVisualStyleBackColor = true;
+            this.btn_Lisapilt.Click += new System.EventHandler(this.btn_Lisapilt_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1032, 407);
+            this.ClientSize = new System.Drawing.Size(1082, 588);
+            this.Controls.Add(this.btn_Lisapilt);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_Delete);
             this.Controls.Add(this.btn_Update);
-            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.btn_insert);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Hindtxt);
             this.Controls.Add(this.Kogustxt);
             this.Controls.Add(this.Toodetxt);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tootetableBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.toodedDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,17 +220,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_insert;
         private System.Windows.Forms.Button btn_Update;
         private System.Windows.Forms.Button btn_Delete;
         private System.Windows.Forms.DataGridView dataGridView1;
         private ToodedDataSet toodedDataSet;
         private System.Windows.Forms.BindingSource tootetableBindingSource;
         private ToodedDataSetTableAdapters.TootetableTableAdapter tootetableTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn toodenimetusDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn kogusDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn hindDataGridViewTextBoxColumn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Lisapilt;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +16,11 @@ namespace andmebaasid
     {
         SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =|DataDirectory|\AppData\Tooded.mdf; Integrated Security = True");
         SqlCommand command;
-        SqlDataAdapter adapter;
+		SqlDataAdapter adapter;
         int Id = 0;
         public Form1()
         {
             InitializeComponent();
-            DisplayData();
         }
 
         private void DisplayData()
@@ -34,6 +34,7 @@ namespace andmebaasid
         }
         private void ClearData()
         {
+            Id = 0;
             Toodetxt.Text = "";
             Kogustxt.Text = "";
             Hindtxt.Text = "";
@@ -47,44 +48,26 @@ namespace andmebaasid
            this.tootetableTableAdapter.Fill(this.toodedDataSet.Tootetable);
 
         }
+        
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void btn_Insert_Click(object sender, EventArgs e)
-        {
-            if (Toodetxt.Text!=""&& Kogustxt.Text!=""&&Hindtxt.Text!="")
-            {
-                command = new SqlCommand("insert into Tootetable(Toodenimetus,Kogus,Hind) values(@toode,@kogus,@hind)",connect);
-                connect.Open();
-                command.Parameters.AddWithValue("@toode", Toodetxt.Text);
-                command.Parameters.AddWithValue("@kogus", Kogustxt.Text);
-                command.Parameters.AddWithValue("@hind", Hindtxt.Text);
-                command.ExecuteNonQuery();
-                connect.Close();
-                ClearData();
-                DisplayData();
-                MessageBox.Show("Andmed on lisatud");
-
-            }
-            else
-            {
-                MessageBox.Show("Viga");
-            }
-        }
+        
 
         private void btn_Update_Click(object sender, EventArgs e)
         {
             if (Toodetxt.Text != "" && Kogustxt.Text != "" && Hindtxt.Text != "")
             {
-                command = new SqlCommand("UPDATE Tootetable SET Toodenimetus=@toode,Kogus=@kogus,Hind=@hind WHERE ID=@id", connect);
+                command = new SqlCommand("UPDATE Tootetable SET Toodenimetus=@toode,Kogus=@kogus,Hind=@hind,Pilt=@pilt WHERE ID=@id", connect);
                 connect.Open();
                 command.Parameters.AddWithValue("@id", Id);
                 command.Parameters.AddWithValue("@toode", Toodetxt.Text);
                 command.Parameters.AddWithValue("@kogus", Kogustxt.Text);
-                command.Parameters.AddWithValue("@hind", Hindtxt.Text);
+                command.Parameters.AddWithValue("@hind", Hindtxt.Text.Replace(",", "."));
+                command.Parameters.AddWithValue("@pilt", save.FileName + save.Filter);
                 command.ExecuteNonQuery();
                 connect.Close();
                 ClearData();
@@ -104,6 +87,76 @@ namespace andmebaasid
             Toodetxt.Text=dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
             Kogustxt.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
             Hindtxt.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            pictureBox1.Image = Image.FromFile(@"C:\Users\morgo\source\repos\DB\images\" + dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString());
         }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            if (Toodetxt.Text != "" && Kogustxt.Text != "" && Hindtxt.Text != "")
+            {
+                command = new SqlCommand("insert into Tootetable(Toodenimetus,Kogus,Hind,Pilt) values(@toode,@kogus,@hind,@pilt)", connect);
+                connect.Open();
+                command.Parameters.AddWithValue("@toode", Toodetxt.Text);
+                command.Parameters.AddWithValue("@kogus", Kogustxt.Text);
+                command.Parameters.AddWithValue("@hind", Hindtxt.Text);
+                command.Parameters.AddWithValue("@pilt", save.FileName + save.Filter);
+                command.ExecuteNonQuery();
+                connect.Close();
+                ClearData();
+                DisplayData();
+                MessageBox.Show("Andmed on lisatud");
+
+            }
+            else
+            {
+                MessageBox.Show("Viga");
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            if (Id != 0)
+            {
+                command = new SqlCommand("DELETE Tootetable WHERE Id=@id", connect);
+                connect.Open();
+                command.Parameters.AddWithValue("@id", Id);
+
+                command.ExecuteNonQuery();
+                connect.Close();
+                DisplayData();
+                ClearData();
+
+                MessageBox.Show("Andmed on lisatud");
+
+            }
+            else
+            {
+                MessageBox.Show("Viga kustutamisega");
+            }
+        }
+        SaveFileDialog save;
+
+        private void btn_Lisapilt_Click(object sender, EventArgs e)
+        {
+            if (Id != 0)
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files (*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    SaveFileDialog save = new SaveFileDialog();
+                    save.FileName = Toodetxt.Text + "_" + Id;
+                    save.Filter = "Image Files (*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+                    save.InitialDirectory = Path.GetFullPath(@"C:\Users\morgo\source\repos\DB\images\");
+                    save.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Viga");
+            }
+
+        }
+        
     }
 }
